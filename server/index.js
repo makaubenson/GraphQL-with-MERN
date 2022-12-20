087c6f61eb9e7004180b3d45e9cb8c342f6497f3
benson <bensonmakau2000@gmail.com>
2022-12-20 06:48:06 +0300

    connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//connect to MongoDB
+connectDB();
 
 app.use(
   "/graphql",
